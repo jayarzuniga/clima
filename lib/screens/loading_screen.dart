ccb1886420dedec1ef98ce8d132a1f0084a5c660
Jay-r <jhayar.zuniga004@gmail.com>
2024-09-07 13:02:17 +0800

    finishing the GPS Locator function

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,31 +1,59 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 
 class LoadingScreen extends StatefulWidget {
-
-  final LocationSettings locationSettings = const LocationSettings(
-    accuracy: LocationAccuracy.high,
-    distanceFilter: 100,
-  );
-
-  Future<void> getLocation () async {
-    Position position = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
-  }
-
   @override
   _LoadingScreenState createState() => _LoadingScreenState();
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  //Settings of the GEOLOCATOR before initialization
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.low,
+    distanceFilter: 100,
+  );
+
+  // Function to get the location of the phone
+  Future<void> getLocation() async {
+    LocationPermission permission = await Geolocator.requestPermission();
+
+    if (permission == LocationPermission.denied || permission == LocationPermission.deniedForever) {
+      if (kDebugMode) {
+        print('Location permission denied');
+      }
+      return;
+    }
+
+    Position position = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+
+    if (kDebugMode) {
+      print(position);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
+
           onPressed: () {
-            //Get the current location
+            getLocation();
           },
-          child: const Text('Get Location'),
+          style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all(Colors.lightBlueAccent.shade700),
+            minimumSize: WidgetStatePropertyAll(Size(150.0, 80.0)),
+          ),
+
+
+          child: const Text(
+              'Get Location',
+            style: TextStyle(
+              fontSize: 30.0
+            ),
+
+          ),
         ),
       ),
     );
