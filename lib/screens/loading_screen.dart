1381fbadd7039905845ceb91b04ac548f5f804a4
Jay-r <jhayar.zuniga004@gmail.com>
2024-09-09 21:59:02 +0800

    refactoring the API request.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,8 +1,9 @@
+import 'package:clima/services/networking.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
+import 'package:clima/services/networking.dart';
+
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -11,54 +12,26 @@ class LoadingScreen extends StatefulWidget {
 
 class _LoadingScreenState extends State<LoadingScreen> {
 
+  String apiKey = '36e94121d059ae358b36c7585e6999d2';
+
   @override
   void initState () {
     super.initState();
-    getLocation();
-    getData();
+    getLocationData();
   }
 
   // Function to get the location of the phone
-  Future<void> getLocation() async {
+  Future<void> getLocationData() async {
     Location location = Location();
     await location.getCurrentLocation();
-    if (kDebugMode) {
-      print(location.latitude);
-    }
-    if (kDebugMode) {
-      print(location.longitude);
-    }
-  }
 
-  void getData() async {
-    var latitude = 14.64;
-    var longitude = 23.02;
-    String apiKey = '36e94121d059ae358b36c7585e6999d2';
+    double latitude = location.latitude;
+    double longitude = location.longitude;
 
-    Uri url = Uri.parse(
-        'https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey');
+    NetworkHelper networkHelper =
+      NetworkHelper("https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apiKey");
 
-    // Await the response
-    http.Response res = await http.get(url);
-
-    jsonDecode(res.body);
-
-    // Check if the response was successful
-    if (res.statusCode == 200) {
-      String data = res.body;
-
-      double temperature = jsonDecode(data)['main']['temp'];
-      int condition = jsonDecode(data)['weather'][0]['id'];
-      String cityName = jsonDecode(data)['name'];
-
-      if (kDebugMode) {
-        print(cityName);
-      }
-    } else {
-      if (kDebugMode) {
-        print('Failed to load data: ${res.statusCode}');
-      }
-    }
+    var weatherData = await networkHelper.getData();
   }
 
 
---
+++ lib/services/networking.dart
@@ -0,0 +1,28 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+
+class NetworkHelper {
+
+  NetworkHelper(this.weatherUrl){
+    networkUrl = Uri.parse(weatherUrl);
+  }
+
+  final String weatherUrl;
+  late final Uri networkUrl;
+
+  Future getData() async {
+    http.Response res = await http.get(networkUrl);
+
+    if (res.statusCode == 200) {
+      String data = res.body;
+
+      return jsonEncode(data);
+
+    } else {
+      if (kDebugMode) {
+        print(res.statusCode);
+      }
+    }
+  }
+}
